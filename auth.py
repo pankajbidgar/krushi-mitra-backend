e71ea7504922216232fc0fb7077d4fef54af2620
unknown <pankajbidgar07@gmail.com>
2026-04-22 17:21:14 +0530

    fix: bcrypt + password length issue

--- auth.py
+++ auth.py
@@ -18,12 +18,19 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
-def verify_password(plain_password,hashed_password):
-    return pwd_context.verify(plain_password,hashed_password)
+# def verify_password(plain_password,hashed_password):
+#     return pwd_context.verify(plain_password,hashed_password)
+def verify_password(plain_password, hashed_password):
+    plain_password = plain_password[:72]  # 🔥 fix
+    return pwd_context.verify(plain_password, hashed_password)
 
 
-def get_Password_hashed(password):
+# def get_Password_hashed(password):
+#     return pwd_context.hash(password)
+def get_password_hash(password):
+    password = password[:72]
     return pwd_context.hash(password)
+
 
 def create_access_token(data:dict):
     to_encode = data.copy()
